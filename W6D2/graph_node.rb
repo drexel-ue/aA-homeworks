8dc86d05664c4616a92a41c5dd770686ca7f9438
ikesh pack <packappsforyou@gmail.com>
2019-05-28 02:02:03 -0400

    Added bfs method and ran successful test

--- W6D2/graph_node.rb
+++ W6D2/graph_node.rb
@@ -8,3 +8,14 @@ class GraphNode
     end
 
 end
+
+def bfs(starting_node, target_value, visited = Set.new())
+    return starting_node if starting_node.val = target_value
+    return nil if visited.include?(starting_node.to_sym)
+
+    visited.add(starting_node.to_sym)
+
+    starting_node.neighbors.each do |neighbor| 
+        bfs(neighbor, target_value, visited)
+    end
+end
