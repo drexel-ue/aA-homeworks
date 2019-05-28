b8a21974bae4746f1eed6959fd157d06759048b8
ikesh pack <packappsforyou@gmail.com>
2019-05-28 07:44:15 -0400

    Corrected map class

--- W6D1/map.rb
+++ W6D1/map.rb
@@ -1,23 +1,38 @@
 class Map
-
     def initialize
-        @underlying_arr = []
+      @underlying_array = []
     end
-
+  
     def set(key, value)
-        @underlying_arr << [key, value]
+      pair_index = underlying_array.index { |pair| pair[0] == key }
+      if pair_index
+        underlying_array[pair_index][1] = value
+      else
+        underlying_array.push([key, value])
+      end
+      value
     end
-
+  
     def get(key)
-        @underlying_arr[@underlying_arr.index { |pair| pair[0] == key }]
+      underlying_array.each { |pair| return pair[1] if pair[0] == key }
+      nil
     end
-
+  
     def delete(key)
-        @underlying_arr.delete_if { |pair| pair[0] == key }
+      value = get(key)
+      underlying_array.reject! { |pair| pair[0] == key }
+      value
     end
-    
+  
     def show
-        @underlying_arr
+      deep_dup(underlying_array)
     end
-
-end
+  
+    private
+  
+    attr_reader :underlying_array
+    
+    def deep_dup(arr)
+      arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+    end  
+  end
