f083cd8f45dac4ffa437c4dd66860cc865d89eea
ikesh pack <packappsforyou@gmail.com>
2019-05-30 07:55:52 -0400

    Defined make_move

--- W6D4/mancala/lib/board.rb
+++ W6D4/mancala/lib/board.rb
@@ -16,6 +16,17 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos].clear
+    cup_idx += 1
+    cup_idx = 0 if cup_idx > 13
+    if cup_idx == 6
+      @cups[6] << stones.pop if current_player_name == @name1
+    elsif cup_idx == 13
+      @cups[13] << stones.pop if current_player_name == @name2
+    else
+      @cups[cup_idx] << stones.pop
+    end
   end
 
   def next_turn(ending_cup_idx)
