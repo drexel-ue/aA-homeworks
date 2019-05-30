8f4acd4d61b177da741aace9ec8ccaef354a4b5e
ikesh pack <packappsforyou@gmail.com>
2019-05-30 08:04:31 -0400

    Defined make_move

--- W6D4/mancala/lib/board.rb
+++ W6D4/mancala/lib/board.rb
@@ -2,12 +2,13 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1, @name2 = name1, name2
     @cups = Array.new(14) { Array.new }
-    (0..13).each { |index| 4.times { @cups[index] << :stone } unless index == 6 || index == 13 }
+    place_stones
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    (0..13).each { |index| 4.times { @cups[index] << :stone } unless index == 6 || index == 13 }
   end
 
   def valid_move?(start_pos)
@@ -17,20 +18,31 @@ class Board
 
   def make_move(start_pos, current_player_name)
     stones = @cups[start_pos]
-    @cups[start_pos].clear
-    cup_idx += 1
-    cup_idx = 0 if cup_idx > 13
-    if cup_idx == 6
-      @cups[6] << stones.pop if current_player_name == @name1
-    elsif cup_idx == 13
-      @cups[13] << stones.pop if current_player_name == @name2
-    else
-      @cups[cup_idx] << stones.pop
+    @cups[start_pos] = []
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
     end
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
-    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
